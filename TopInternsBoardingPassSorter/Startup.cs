a7d4081045b14dccdd2a5e27a34f7761f8432e37
Tariq Senosy <tariqsenosy@gmail.com>
2021-11-17 14:17:41 +0200

    Adding Swagger

--- TopInternsBoardingPassSorter/Startup.cs
+++ TopInternsBoardingPassSorter/Startup.cs
@@ -1,19 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TopInternsBoardingPassSorter.Application.Logic;
 using TopInternsBoardingPassSorter.Domain.Models.TopInternsTransportationCards;
 using TopInternsBoardingPassSorter.Services;
 using TopInternsBoardingPassSorter.Services.TopInternsBusService;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace TopInternsBoardingPassSorter
 {
@@ -32,6 +27,22 @@ namespace TopInternsBoardingPassSorter
             services.AddControllers();
             services.AddTransient<ITopInternsBusService, TopInternsBusService>();
             services.AddTransient<ITopInternsTransCardsSorter<TopInternsTransportationCard>, TopInternsTransCardsSorter>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "TopInterns API",
+                    Version = "v1",
+                    Description = "Sorting transportation cards algorithm.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Tariq Senosy",
+                        Email = "tariqsenosy@gmail.com"
+                    },
+                });
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +52,18 @@ namespace TopInternsBoardingPassSorter
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TopInterns API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                c.RoutePrefix = string.Empty;
+            });
+
 
             app.UseHttpsRedirection();
 
